673d2c8b9e6519f7f6f2138a13cab9650417e3fd
André Pereira <andrepereira97@yahoo.com>
2022-11-26 21:27:15 -0300

    :sparkles: add FileWriter and FileReader examples

---
+++ io/src/FileReaderTest01.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest01 {
+  public static void main(String[] args) {
+    File file = new File("io/txt/file.txt");
+
+    try(FileReader fileReader = new FileReader(file)) {
+//      char[] in = new char[20];
+//      int read = fileReader.read(in);
+//      for(char c: in) {
+//        System.out.print(c);
+//      }
+
+      int i;
+
+      while((i = fileReader.read()) != -1) {
+        System.out.print((char) i );
+      }
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+  }
+}
